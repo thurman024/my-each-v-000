96b938924e66a802a697a607280b3ed67468ee52
Stephen Thurman <thurman024@gmail.com>
2018-01-25 14:20:06 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(argument)
+  counter = 0
+  while counter < argument.length
+    yield argument[counter]
+    counter += 1
+  end
+  argument
 end
